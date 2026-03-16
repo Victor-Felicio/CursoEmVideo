12184731d5310a9d58a83ade67125daa22a0c135
victor <victor.adam.tankian@gmail.com>
2026-03-16 09:21:31 -0300

    Desafios da aula 1 do Curso em Video.

--- curso_em_video/aula001/desafio_03.py
+++ curso_em_video/aula001/desafio_03.py
@@ -1,3 +1,5 @@
-num1 = int(input("Primeiro número "))
-num2 = int(input("Segundo número "))
-print("A soma é ",num1+num2)
+num1 = int(input("Digite um número: "))
+num2 = int(input("Digite outro número: "))
+soma = num1 + num2
+
+print("O resultado da soma de {} e {} é igual a {}.".format(num1, num2, soma))
