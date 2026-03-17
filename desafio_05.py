b621192521267f84537b5d7db049e407f21107c0
Victor-Felicio <victor.adam.tankian@gmail.com>
2026-03-17 17:43:57 -0300

    Add files via upload

--- desafio_05.py
+++ desafio_05.py
@@ -1,6 +1,3 @@
 num = int(input("Digite um número: "))
 
-ant = num - 1
-suc = num + 1
-
-print("O número digitado foi {}, seu antecessor é o número {} e o seu sucessor é o número {}.".format(num, ant, suc))
+print("O número digitado foi {}, seu antecessor é o número {} e o seu sucessor é o número {}.".format(num, (num-1), (num+1)))
