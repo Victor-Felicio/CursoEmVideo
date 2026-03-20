ef4ca1b359ce142ace83742a33f188e257a68b1f
victor <victor.adam.tankian@gmail.com>
2026-03-20 09:51:40 -0300

    Calculadora de média.

---
+++ curso_em_video/aula001/desafio_07.py
@@ -0,0 +1,7 @@
+nome = input("Nome do aluno: ")
+nota1 = float(input("Primeira nota do aluno: "))
+nota2 = float(input("Segunda nota do aluno: "))
+
+media = (nota1 + nota2) / 2
+
+print("A média do aluno {} é de {}.".format(nome, media))
